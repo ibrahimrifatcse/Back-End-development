6a92611d6eec84d560b166016fe230cede2e2a3c
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-08-02 23:02:14 +0600

    Create string-reversing.py

---
+++ Programming in python/string-reversing.py
@@ -0,0 +1,16 @@
+# str[start:stop:step]
+trial = 'reversal'
+new_trial = trial[::-1]
+
+print(new_trial)
+
+#--------------------------------
+def str_reverse(str):
+    if len(str) == 0:
+        return str
+    
+    else:
+        return str_reverse(str[1:]) + str[0]
+    
+    
+print(str_reverse('miharbi'))    
